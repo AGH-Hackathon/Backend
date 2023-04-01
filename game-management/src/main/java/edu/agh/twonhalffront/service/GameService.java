85f6b2f8862298c92e69c5b1bf1229749d32266f
Rafał Kamiński <r.kaminski008@gmail.com>
2023-04-01 12:43:45 +0200

    Remove src and introduce game-management module

---
+++ game-management/src/main/java/edu/agh/twonhalffront/service/GameService.java
@@ -0,0 +1,7 @@
+package edu.agh.twonhalffront.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GameService {
+}
--- src/main/java/agh/edu/pl/backend/dto/ImageDescriptionMatch.java
+++
@@ -1,8 +0,0 @@
-package agh.edu.pl.backend.dto;
-
-import java.util.UUID;
-
-public class ImageDescriptionMatch {
-    UUID imageId;
-    UUID descriptionId;
-}
