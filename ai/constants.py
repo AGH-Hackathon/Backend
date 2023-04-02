c665583e33f0b6ee39939e7f8831a66cd777b001
Tomasz Ostafin <ostafintomasz1@gmail.com>
2023-04-02 13:55:28 +0200

    Remove API keys file from being followed by git

--- ai/constants.py
+++ ai/constants.py
@@ -2,6 +2,6 @@ from typing import Final
 
 OPEN_AI_API_KEY: Final[str] = ""
 
-CLOUDINARY_CLOUD_NAME: Final[str] = "dcbnhbnvk"
-CLOUDINARY_API_KEY: Final[str] = "899829585613519"
-CLOUDINARY_API_SECRET: Final[str] = "jAv1FNfRJmgQ8PU4rcEwyWo4HjU"
+CLOUDINARY_CLOUD_NAME: Final[str] = ""
+CLOUDINARY_API_KEY: Final[str] = ""
+CLOUDINARY_API_SECRET: Final[str] = ""
